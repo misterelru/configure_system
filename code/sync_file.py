3870cf08d624f61ab4d9a05ef22a823bbfa6ee6c
alekseyosipov <aleksey.osipov@piano.io>
2023-08-23 18:26:35 +0400

    Add arg checking and logging

--- code/sync_file.py
+++ code/sync_file.py
@@ -10,7 +10,8 @@ import sys
 from sys import argv
 
 def sync_files(source_file_from_cloud: str) -> None:
-    '''Compare local DB file with DB file fom GoogleDrive'''
+    '''Compare local DB file with DB file fom GoogleDrive,
+    sync if files different'''
     path_to_this_module = os.path.dirname(os.path.realpath(__file__))
 
     synced_database_file = os.path.join(path_to_this_module, 'Database.kdbx')
@@ -20,10 +21,18 @@ def sync_files(source_file_from_cloud: str) -> None:
         size_source_file = os.stat(source_file_from_cloud).st_size
         if size_source_file != size_synced_file:
             shutil.copy(source_file_from_cloud, synced_database_file)
+            print("File has been sync:", synced_database_file)
+            sys.stdout.flush()
     else:
         sys.exit(2)
 
 while True:
-    SOURCE_FILE_FROM_CLOUD = argv[1]
-    sync_files(SOURCE_FILE_FROM_CLOUD)
-    time.sleep(3600)
+    if sys.argv[1:]:
+        SOURCE_FILE_FROM_CLOUD = argv[1]
+        sync_files(SOURCE_FILE_FROM_CLOUD)
+        time.sleep(3600)
+    else:
+        print("No source DB file specified, specify the source file.\n"
+              "Example: sync_file.py path_to_source_DB_file")
+        sys.stdout.flush()
+        sys.exit(2)
